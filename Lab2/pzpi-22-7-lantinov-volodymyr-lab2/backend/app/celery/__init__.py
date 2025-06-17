70546832110ba17a40569998a77fb3a72b57eb97
NureLantinovVolodymyr <volodymyr.lantinov@nure.ua>
2025-06-18 01:55:20 +0300

    Delete Lab2 directory

--- Lab2/pzpi-22-7-lantinov-volodymyr-lab2/backend/app/celery/__init__.py
+++
@@ -1,2 +0,0 @@
-from .calery_app import celery_app
-# from .periodic_task import setup_periodic_tasks
--- Lab2/pzpi-22-7-lantinov-volodymyr-lab2/backend/app/database/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_model import Base
-from .conn import engine, url, get_db
-from .models import User, Warehouse, Rental, Payment, Lock, Message, UserRole, PremiumService, PaymentStatus, RentalStatus
